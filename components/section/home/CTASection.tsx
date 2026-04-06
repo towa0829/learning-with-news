575078e82934a50bc06b1147474dbc81cc2391f4
towa0829 <towa0000829@icloud.com>
2026-04-06 12:10:52 +0900

    feat: add CTA section

---
+++ components/section/home/CTASection.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { FaArrowRightLong } from "react-icons/fa6";
+
+const CTASection = () => {
+  return (
+    <section className="flex flex-col items-center text-center py-30">
+      <div className="bg-blue-50 rounded-xl py-10 px-20">
+        <h2 className="text-3xl font-bold">Ready to Improve Your English?</h2>
+        <p className="text-base text-muted-foreground mt-4 mb-6">Start reading real news articles and building your vocabulary today!</p>
+        <Button asChild className="bg-blue-600! hover:bg-blue-700! text-white! rounded-sm py-5 px-6">
+          <Link href="/learn">Start Reading<FaArrowRightLong /></Link>
+        </Button>
+      </div>
+    </section>
+    )
+}
+
+export default CTASection
