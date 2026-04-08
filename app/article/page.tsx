f8f6c0dcca447dfe35ac99259e380bfea2f83441
towa0829 <towa0000829@icloud.com>
2026-04-08 15:45:01 +0900

    feat: add category button

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -1,9 +1,16 @@
+'use client';
 import ArticleCardList from "@/components/parts/article/ArticleCardList";
 import { FaHistory } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Category } from "@/lib/type";
+import { useState } from "react";
+
+const CategoryList: Category[] = ["general", "business", "entertainment", "health", "science", "sports", "technology"];
+
 
 const ArticleListPage = () => {
+  const [selectedCategory, setSelectedCategory] = useState<Category>("general");
   return (
     <main className="mx-auto max-w-6xl px-4 py-12 sm:px-6 sm:py-15">
       <div className="flex items-center justify-between">
@@ -14,10 +21,24 @@ const ArticleListPage = () => {
           </Link>
         </Button>
       </div>
-      <p className="mb-8 text-base text-muted-foreground sm:text-lg">
+      <p className="mb-6 text-base text-muted-foreground sm:text-lg">
         Browse English news articles and expand your vocabulary
       </p>
-      <ArticleCardList />
+      <div className="mb-3 flex flex-wrap items-center gap-1">
+        {CategoryList.map((category) => (
+          <Button
+            key={category}
+            variant={selectedCategory === category ? "default" : "outline"}
+            className="mr-2 mb-2"
+            onClick={() => setSelectedCategory(category)}
+          >
+            {category}
+          </Button>
+        ))}
+      </div>
+      <ArticleCardList 
+        category={selectedCategory}
+      />
 
     </main>
   )
